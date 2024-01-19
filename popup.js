359f538da155bdf1ddec67a65b633bf8cee98ee1
kartik <kartikth40@gmail.com>
2024-01-19 16:21:36 +0530

    added tab system

--- popup.js
+++ popup.js
@@ -1,5 +1,17 @@
-const runBtn = document.querySelector('.run')
+const tabNames = ['focus', 'settings']
+const tabs = tabNames.map(tabName => ({
+  btn: document.querySelector(`.${tabName}-tab-btn`),
+  tab: document.querySelector(`.${tabName}-tab`)
+}))
+
+const runBtn = document.querySelector('.focus-btn')
 const timer = document.querySelector('.timer')
+
+const focusTabBtn = document.querySelector('.focus-tab-btn')
+const settingsTabBtn = document.querySelector('.settings-tab-btn')
+
+const focusTab = document.querySelector('.focus-tab')
+const settingsTab = document.querySelector('.settings-tab')
 
 const settingsForm = document.querySelector('#settings-form')
 
@@ -7,7 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
   runBtn.addEventListener('click', () => {
     chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
       chrome.runtime.sendMessage({startTimer: true});
-      window.close();
+      // window.close();
     });
   });
 });
@@ -18,6 +30,22 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   }
 });
 
+tabs.forEach(({tab, btn}) => {
+  btn.addEventListener('click', () => {
+    if(btn.classList.contains('active')) return
+    tabs.forEach(curTab => {
+      if(curTab.btn.classList.contains('active')) {
+        curTab.btn.classList.remove('active')
+      }
+      if(curTab.tab.classList.contains('active')) {
+        curTab.tab.classList.remove('active')
+      }
+    })
+    btn.classList.add('active')
+    tab.classList.add('active')
+  })
+})
+ 
 settingsForm.addEventListener('submit', (e) => {
   e.preventDefault()
 })
